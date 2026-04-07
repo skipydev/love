e3b7b61c71ec0f7d6dd52ada3370023d24a42053
skipy <skipyy1@proton.me>
2026-04-08 01:15:00 +0300

    Updates

--- _next/static/5CsUU8cp4fGHX8IAWk69i/_buildManifest.js
+++ _next/static/5CsUU8cp4fGHX8IAWk69i/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/love//_next/:path+",
-        "destination": "/love/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
